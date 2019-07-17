3bb952c8f355992bc9e24a7f0532a0f0a594b9c9
leechangyo <48198730+leechangyo@users.noreply.github.com>
2019-07-17 17:24:23 +0900

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -12,10 +12,16 @@ class KalmanFilterToy:
         self.prev_x = 0
         self.prev_t = 0
     def predict(self,t):
-        prediction = 0
-        return prediction
+        return self.v*(t-self.prev_time) + self.prev_x
     def measure_and_update(self,x,t):
+        measured_v = (x-self.prev_x)/(t-self.prev_time)
+        self.v + = 0.5*(measured_v - self.v)
+        # 0.5 is the weight, so to need to find out which weight is best to reduce error(Deep Learning idea)
+        
+        self.prev_x =x
+        self.prev_time = t
         return
+    # 내생각에는 그냥 밸류를 사용하거나 업데이트 시키는 것이다.
 
 
 sim_run(options,KalmanFilterToy)
